7b92026dd60b17cf44eac35f546a9fb93aa7e11e
gulu42 <aditya.gulati@iiitb.org>
2018-11-04 16:25:05 +0530

    generated combined data

--- extra_stuff/try_pickle.py
+++ extra_stuff/try_pickle.py
@@ -1,9 +1,7 @@
 import pickle
 import sys
 
-print sys.getdefaultencoding()
-print type(open("dictionary.pickle","rb"))
 with open("dictionary.pickle","rb") as input_file:
-    e = pickle.load(input_file)
+    e = pickle.load(input_file,encoding="latin1")
 
-print e
+print(e)
